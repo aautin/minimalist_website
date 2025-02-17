86c67d9078daf3ca09e53e9f77a181fe10f593f9
aautin.home <alexandreautin.aa@gmail.com>
2025-02-17 17:51:39 +0100

    add dist repo (js compiled files from ts)

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+function printName() {
+    const name = document.getElementById("name").value;
+    nameDisplay.textContent = `Hello, ${name}!`;
+    nameDisplay.style.display = "flex";
+    document.getElementById("submitButton").style.display = "none";
+    document.getElementById("label").style.display = "none";
+    document.getElementById("name").style.display = "none";
+}
+const button = document.getElementById("submitButton");
+button.addEventListener("click", printName);
+const nameDisplay = document.getElementById("nameDisplay");
