d0e6e552bd7bb40b4507732bf415a24fa5fd5e55
aautin <ezagof@gmail.com>
2025-02-17 11:36:36 +0100

    receive and console.log username and password inputs

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-function switchPanel(index: number) {
+function switchPanel(name: string) {
     // Hide panels
     document.querySelectorAll('.panel').forEach(panel => {
         panel.classList.remove('active');
@@ -10,6 +10,24 @@ function switchPanel(index: number) {
 
 
     // Show selected panel and btn
-    document.getElementById(`panel${index}`).classList.add('active');
-    document.getElementById(`btn${index}`).classList.add('active');
+    document.getElementById(`${name}Panel`).classList.add('active');
+    document.getElementById(`${name}Btn`).classList.add('active');
+}
+
+function login() {
+    const username = document.getElementById('username') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+
+    console.log("LOGIN", username.value, password.value);
+    username.value = "";
+    password.value = "";
+}
+
+function register() {
+    const username = document.getElementById('username') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+
+    console.log("REGISTER", username.value, password.value);
+    username.value = "";
+    password.value = "";
 }
