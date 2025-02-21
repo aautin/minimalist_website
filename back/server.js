2e92544e62d479450b01a248373ee94cc9347470
aautin <alexandreautin.aa@gmail.com>
2025-02-21 19:04:30 +0100

    use live-server for front, restart on empty running website

--- back/server.js
+++ back/server.js
@@ -7,16 +7,6 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.post("/name-length", (req, res) => {
-    const name = req.body.name;
-
-    if (typeof name !== "string") {
-        return res.status(400).json({ error: "Invalid input, please send a string" });
-    }
-
-    res.json({ length: name.length });
-});
-
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
---
+++ front/dist/script.js
@@ -0,0 +1,6 @@
+function login() {
+    // const username = (document.getElementById("username")! as HTMLInputElement).textContent;
+    // if (username && await )
+}
+// function search() {
+// }
